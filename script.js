89c8b788eb3c5598afe10541c76e9a4714841117
MatildaG <matildakgriffiths@gmail.com>
2024-04-05 23:17:53 +1300

    Adding multiple instances of rubbish + more rubbish option when finished. Wash functionality added

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ class Item {
         this.correctBin = correctBin;
         this.cleaned = cleaned;
     }
-}
+}   
+
+
+//Array of image references
+//0 = image reference, 1 = correct bin, 2 = cleaned/not, 3 = trash type, 4 = recyclable/not
+let rubbishArray = [
+    ["Images/rubbish.png", "rubbishBin", 1, "Rubbish"],
+    ["Images/plastic.png", "recycleBin", 1, "Plastic"],
+    ["Images/rubbish.png", "rubbishBin", 1, "Rubbish"],
+    ["Images/plastic.png", "recycleBin", 1, "Plastic"],
+    ["Images/rubbish.png", "rubbishBin", 1, "Rubbish"]
+];
 
 // Array of bins
 let bins = [
@@ -15,17 +26,46 @@ let bins = [
     document.getElementById("foodScrapBin")
 ];
 
-// Items array
-let items = [
-    new Item(document.getElementById("rubbish"), "rubbishBin", 1),
-    new Item(document.getElementById("plastic"), "recycleBin", 1)
-];
+//Water bucket
+let waterBucket = document.getElementById("waterBucket");
 
 // Users points
 let points = 0;
 
-// Creates a drag function for each item
-items.forEach(dragElement);
+//Counter for how many trash items are currently displayed
+let counter = 0;
+
+/**
+ * generateNewRubbish - This function is called onload and on nextButton click 
+ * and it creates 5 new trash items adding to DOM
+ */
+//Function called onload and on nextButton click which creates 5 new trash items
+function generateNewRubbish(){
+    document.getElementById("nextButton").hidden = true;
+
+    //Loop through 5x to add new rubbish items
+    let itemsDiv = document.getElementById("items");
+    let items = [];
+    counter = 5;
+    for(let i = 0; i < 5; i++){
+        //Create rubbish div
+        let elementDiv = document.createElement("div");
+        elementDiv.setAttribute("id", "rubbishDiv" + i);
+        let rubbishPiece = document.createElement("img");
+        rubbishPiece.setAttribute("src", rubbishArray[i][0]);
+        rubbishPiece.setAttribute("width", "70px");
+        rubbishPiece.setAttribute("id", "rubbishPiece" + i);
+        rubbishPiece.setAttribute("class", "rubbishClass");
+        rubbishPiece.setAttribute("alt", rubbishArray[i][3]);
+
+        //Add to the DOM
+        elementDiv.appendChild(rubbishPiece);
+        itemsDiv.appendChild(elementDiv);
+        items[i] = new Item(elementDiv, rubbishArray[i][1], rubbishArray[i][2]);
+    }
+    // Creates a drag function for each item
+    items.forEach(dragElement);
+}
 
 /**
  * dragElement - This function allows an item to be dragged, 
@@ -73,13 +113,36 @@ function dragElement(item) {
      * placed in a bin (if so, check if the bin is correct).
      */
     function closeDragElement() {
-        let check = isInsideBin(item);
+        //Event that item is in a bucket
+        let checkClean = isInWaterBucket(item);
+        if(checkClean){
+            item.cleaned = 1;
+            alert("cleaned");
+        }
+        else{
+            let check = isInsideBin(item);
 
-        if (check) {
-            element.style.display = "none";
-            points++;
-        } else if (check === false) {
-            alert("Wrong Bin");
+            if (check) {
+                //Check item is washed 
+                if(item.cleaned == 0){
+                    alert("Not Clean");
+                }
+                else{
+                    element.style.display = "none";
+                    points++;
+                    counter--;
+                    //Check if there are any more elements on the page
+                    if(counter == 0){
+                        document.getElementById("nextButton").hidden = false; 
+                        alert("visible");
+                    }
+                    else{
+                        document.getElementById("nextButton").hidden = true; 
+                    }
+                }
+            } else if (check === false) {
+                alert("Wrong Bin");
+            }
         }
 
         document.onmouseup = null;
@@ -112,4 +175,20 @@ function isInsideBin(item) {
     }
 
     return null;
+}
+
+/**
+ * isInWaterBucket - Checks if an item is placed inside the wash bucket.
+ * @param {Item} item - The item we are checking if has been 
+ * placed in the wash bucket.
+ */
+function isInWaterBucket(item) {
+    let element = item.element;
+    if (element.offsetTop >= waterBucket.offsetTop &&
+        (element.offsetTop + element.offsetHeight) <= (waterBucket.offsetTop + waterBucket.offsetHeight) &&
+        element.offsetLeft >= waterBucket.offsetLeft &&
+        (element.offsetLeft + element.offsetWidth) <= (waterBucket.offsetLeft + waterBucket.offsetWidth)) {
+        return true;
+    }
+    return false;
 }
